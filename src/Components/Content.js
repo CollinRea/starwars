4cd886087633262be37b7a40f95db842ef9797b1
Collin Rea <collinr01@gmail.com>
2018-07-30 06:38:23 -0400

    Move state down to individual content child component and turn Content into a switch function

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import People from './People';
 import Planets from './Planets';
 import Films from './Films';
@@ -6,46 +6,24 @@ import Species from './Species';
 import Vehicles from './Vehicles';
 import Starships from './Starships';
 
-export default class Content extends Component {
-  state = {results: []};
-  componentDidMount(){
-    const url = this.props.match.url;
-    const context = this;
-    async function fetchData() {
-      const response = await fetch('https://swapi.co/api' + url);
-      const data = await response.json();
-      await context.setState({
-        count: data.count, 
-        results: data.results,
-        next: data.next,
-        prev: data.previous
-      });
-    }
-    fetchData();
-  }
-  componentToRender(){
-    const match = this.props.match.params.topic;
-    switch (match) {
-      case 'people':
-        return <People />
-      case 'planets':
-        return <Planets />
-      case 'films':
-        return <Films />
-      case 'species':
-        return <Species />
-      case 'vehicles':
-        return <Vehicles />
-      case 'starships':
-        return <Starships />
-      default:
-        return <h2>No Data Found.</h2>
-    }
-  }
-  render() {
-    let componentToRender = this.componentToRender() || <h2>Loading...</h2>;
-    return (
-      <div>{componentToRender}</div>
-    )
+const Content = (props) => {
+  const match = props.match.params.topic;
+  switch (match) {
+    case 'people':
+      return <People url={match} />
+    case 'planets':
+      return <Planets url={match} />
+    case 'films':
+      return <Films url={match} />
+    case 'species':
+      return <Species url={match} />
+    case 'vehicles':
+      return <Vehicles url={match} />
+    case 'starships':
+      return <Starships url={match} />
+    default:
+      return <h2>No Data Found.</h2>
   }
 }
+
+export default Content;
--- src/Components/Films.js
+++ src/Components/Films.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 export default class Films extends Component {
+  state = {results: []};
+  componentDidMount(){
+    const url = this.props.url;
+    const context = this;
+    async function fetchData() {
+      const response = await fetch('https://swapi.co/api/' + url);
+      const data = await response.json();
+      await context.setState({
+        count: data.count, 
+        results: data.results,
+        next: data.next,
+        prev: data.previous
+      });
+    }
+    fetchData();
+  }
   render() {
     return (
       <div>
         <h2>Films</h2>
+        <p>{this.state.count}</p>
       </div>
     )
   }
